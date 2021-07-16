7db06f1a1d29b4775407f79239565ad78c610e1b
Doğa Budak <dogabudak@gmail.com>
2021-07-16 23:34:48 +0200

    removed unnessesary todo

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import * as nano from 'nanomsg'
 const nanoReq = nano.socket('req');
 // TODO pick it up from config ?
 nanoReq.connect('tcp://127.0.0.1:5608');
-// TODO write test for this
 export const checkToken = async (token) => {
     return new Promise((resolve, reject)=>{
         nanoReq.send('jwt ' + token);
